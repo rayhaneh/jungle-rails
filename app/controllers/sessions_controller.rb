ff9cb017357ac62b43f421ddfe1c5a01e562b978
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-09-04 18:37:02 +0000

    Used a class method for authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,26 @@ class SessionsController < ApplicationController
 
   # Create a Session Cookie (Login)
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       # Save the user id inside the browser cookie.
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login', notice: "Invalid username or password"
+      # If user's login doesn't work, send them back to the login form.
+        redirect_to '/login', notice: "Invalid username or password"
+      end
     end
+
+    # If the user exists AND the password entered is correct.
+    # if user && user.authenticate(params[:password])
+    #   # Save the user id inside the browser cookie.
+    #   session[:user_id] = user.id
+    #   redirect_to '/'
+    # else
+    # # If user's login doesn't work, send them back to the login form.
+    #   redirect_to '/login', notice: "Invalid username or password"
+    # end
+
   end
 
   # Destroy Session Cookie (Logout)
