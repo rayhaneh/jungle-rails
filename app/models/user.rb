3f1400ee99869804f77299ce34570640659b6a33
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-09-04 20:44:11 +0000

    Changed user model validation to cover what is not coverd in has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ActiveRecord::Base
 
+  # When using has_secure_password,
+  # the following validations are added automatically:
+  # - Password must be present on creation
+  # - Password length should be less than or equal to 72 characters
+
   has_secure_password
   has_many :reviews
 
-  validates :name, :email, presence: true
+  validates :name, :email, :password_confirmation, presence: true
   validates :email, uniqueness: { case_sensitive: false }
-  validates :password, length: {minimum: 4}
+  validates :password, length: {minimum: 8}
 
 
   def self.authenticate_with_credentials(email, password)
